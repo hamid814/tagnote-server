011dc4797483d2087e05389ab2e5fdbb54915df4
hamid <hamid331994@gmail.com>
2020-04-05 13:43:02 +0430

    edited Note model

--- db/notes.js
+++ db/notes.js
@@ -3,91 +3,91 @@ let notes = [
     id: 'note_id_1',
     body: 'text',
     tag: '5e821da30215e5390c6e9a54',
-    otherTags: ['5e821da30215e5390c6e9a54', '5e821da30215e5390c6e9a54'],
+    otherTags: ['5e821da30215e5390c6e9a55', '5e821da30215e5390c6e9a56'],
     date: 'date'
   },
   {
     id: 'note_id_2',
     body: 'text number 2',
-    tag: '5e821da30215e5390c6e9a54',
-    otherTags: ['5e821da30215e5390c6e9a54', '5e821da30215e5390c6e9a54', '5e821da30215e5390c6e9a54'],
+    tag: '5e821da30215e5390c6e9a57',
+    otherTags: ['5e821da30215e5390c6e9a56', '5e821da30215e5390c6e9a55', '5e821da30215e5390c6e9a54'],
     date: 'date'
   },
   {
     id: 'note_id_3',
     body: 'text for note 3',
-    tag: '5e821da30215e5390c6e9a54',
-    otherTags: ['5e821da30215e5390c6e9a54', '5e821da30215e5390c6e9a54'],
+    tag: '5e821da30215e5390c6e9a57',
+    otherTags: ['5e821da30215e5390c6e9a56', '5e821da30215e5390c6e9a54'],
     date: 'date'
   },
   {
     id: 'note_id_4',
     body: '4th note text',
     tag: '5e821da30215e5390c6e9a54',
-    otherTags: ['5e821da30215e5390c6e9a54', '5e821da30215e5390c6e9a54'],
+    otherTags: ['5e821da30215e5390c6e9a57', '5e821da30215e5390c6e9a55'],
     date: 'date'
   },
   {
     id: 'note_id_5',
     body: '5th note text',
-    tag: '5e821da30215e5390c6e9a54',
-    otherTags: ['5e821da30215e5390c6e9a54', '5e821da30215e5390c6e9a54'],
+    tag: '5e821da30215e5390c6e9a59',
+    otherTags: ['5e821da30215e5390c6e9a54', '5e821da30215e5390c6e9a56'],
     date: 'date'
   },
   {
     id: 'note_id_6',
     body: '6th note text goes here 6th note text goes here 6th note text goes here 6th note text goes here',
-    tag: '5e821da30215e5390c6e9a54',
-    otherTags: ['5e821da30215e5390c6e9a54', '5e821da30215e5390c6e9a54'],
+    tag: '5e821da30215e5390c6e9a56',
+    otherTags: ['5e821da30215e5390c6e9a54', '5e821da30215e5390c6e9a59'],
     date: 'date'
   },
   {
     id: 'note_id_7',
     body: '7th note text goes here',
-    tag: '5e821da30215e5390c6e9a54',
-    otherTags: ['5e821da30215e5390c6e9a54', '5e821da30215e5390c6e9a54'],
+    tag: '5e821da30215e5390c6e9a55',
+    otherTags: ['5e821da30215e5390c6e9a59', '5e821da30215e5390c6e9a54'],
     date: 'date'
   },
   {
     id: 'note_id_8',
     body: '8th note text goes here',
-    tag: '5e821da30215e5390c6e9a54',
-    otherTags: ['5e821da30215e5390c6e9a54', '5e821da30215e5390c6e9a54'],
+    tag: '5e821da30215e5390c6e9a58',
+    otherTags: ['5e821da30215e5390c6e9a54', '5e821da30215e5390c6e9a59'],
     date: 'date'
   },
   {
     id: 'note_id_9',
     body: '9th note text goes here',
-    tag: '5e821da30215e5390c6e9a54',
-    otherTags: ['5e821da30215e5390c6e9a54', '5e821da30215e5390c6e9a54'],
+    tag: '5e821da30215e5390c6e9a55',
+    otherTags: ['5e821da30215e5390c6e9a58', '5e821da30215e5390c6e9a54'],
     date: 'date'
   },
   {
     id: 'note_id_10',
     body: '10th note text goes here',
     tag: '5e821da30215e5390c6e9a54',
-    otherTags: ['5e821da30215e5390c6e9a54', '5e821da30215e5390c6e9a54'],
+    otherTags: ['5e821da30215e5390c6e9a57', '5e821da30215e5390c6e9a58'],
     date: 'date'
   },
   {
     id: 'note_id_11',
     body: '11th note text goes here 11th note text goes here 11th note text goes here',
-    tag: '5e821da30215e5390c6e9a54',
-    otherTags: ['5e821da30215e5390c6e9a54', '5e821da30215e5390c6e9a54'],
+    tag: '5e821da30215e5390c6e9a58',
+    otherTags: ['5e821da30215e5390c6e9a57', '5e821da30215e5390c6e9a54'],
     date: 'date'
   },
   {
     id: 'note_id_12',
     body: '12th note text goes here',
-    tag: '5e821da30215e5390c6e9a54',
-    otherTags: ['5e821da30215e5390c6e9a54', '5e821da30215e5390c6e9a54'],
+    tag: '5e821da30215e5390c6e9a57',
+    otherTags: ['5e821da30215e5390c6e9a54', '5e821da30215e5390c6e9a58'],
     date: 'date'
   },
   {
     id: 'note_id_13',
     body: '13th note text goes here',
     tag: '5e821da30215e5390c6e9a54',
-    otherTags: ['5e821da30215e5390c6e9a54', '5e821da30215e5390c6e9a54'],
+    otherTags: ['5e821da30215e5390c6e9a58', '5e821da30215e5390c6e9a54'],
     date: 'date'
   }
 ]
