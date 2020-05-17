1ecf043bec2b1b2adf444c0f856ce4641c1f0e42
hamidbakhtiari <hamid331994@gmail.com>
2020-05-18 03:39:59 +0430

    tags/search => tags?find=taxt

--- controllers/tags.js
+++ controllers/tags.js
@@ -1,41 +1,41 @@
-const Tag = require('../models/Tag')
-const asyncHandler = require('../middleware/asyncHandler')
-const ErrorResposne = require('../utils/errorResponse')
+const Tag = require('../models/Tag');
+const asyncHandler = require('../middleware/asyncHandler');
+const ErrorResposne = require('../utils/errorResponse');
 
 // @route     GET /api/v1/tags
 // @desc      get all tags
 exports.getTags = asyncHandler(async (req, res, next) => {
-  const tags = await Tag.find();
+  let query;
+
+  query = Tag.find();
+
+  if (req.query.find) {
+    query = Tag.find({ name: { $regex: req.query.find } });
+  }
+
+  const tags = await query;
 
   res.send({
     success: true,
     count: tags.length,
-    data: tags
-  })
-})
+    data: tags,
+  });
+});
 
 // @route     GET /api/v1/tags/:id
 // @desc      get a single tag with id
 exports.getTag = asyncHandler(async (req, res, next) => {
   const tag = await Tag.findById(req.params.id);
 
-  if(!tag) {
-    return next(
-      new ErrorResposne(`No tag with id ${req.params.id}`, 404)
-    )
+  if (!tag) {
+    return next(new ErrorResposne(`No tag with id ${req.params.id}`, 404));
   }
 
   res.send({
     success: true,
-    data: tag
-  })
-})
-
-// @route    GET /api/v1/tags/search
-// @desc     search in tags
-exports.searchTags = asyncHandler(async (req, res, next) => {
-  res.send('search in tags')
-})
+    data: tag,
+  });
+});
 
 // @route     POST /api/v1/tags
 // @desc      create a new tag
@@ -44,31 +44,29 @@ exports.createTag = asyncHandler(async (req, res, next) => {
 
   res.status(201).json({
     success: true,
-    data: tag
-  })
-})
+    data: tag,
+  });
+});
 
 // @route     DELETE /api/v1/tags/id/:id
 // @desc      delete a tag
 exports.deleteTag = asyncHandler(async (req, res, next) => {
   const tag = await Tag.findById(req.params.id);
 
-  if(!tag) {
-    return next(
-      new ErrorResposne(`No tag with id ${req.params.id}`, 404)
-    )
+  if (!tag) {
+    return next(new ErrorResposne(`No tag with id ${req.params.id}`, 404));
   }
 
-  tag.remove()
+  tag.remove();
 
   res.status(200).json({ success: true, data: {} });
-})
+});
 
 // @route     PUT /api/v1/tags/:id
 // desc       pudate a tag
 exports.updateTag = asyncHandler(async (req, res, next) => {
-  res.send('update tag')
-})
+  res.send('update tag');
+});
 
 /*
 
--- seeder.js
+++ seeder.js
@@ -13,7 +13,7 @@ mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: false,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
 });
 
 // Read fade database files
@@ -49,4 +49,3 @@ if (process.argv[2] === '-i') {
 } else if (process.argv[2] === '-d') {
   deleteData();
 }
-
