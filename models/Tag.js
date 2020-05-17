f0c50ecaebe48f480ce2196a6709511441bc9188
hamidbakhtiari <hamid331994@gmail.com>
2020-05-18 03:39:59 +0430

    tags/search => tags?find=taxt

--- models/Tag.js
+++ models/Tag.js
@@ -1,26 +1,26 @@
-const mongoose = require('mongoose')
-const slugify = require('slugify')
+const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const TagSchema = new mongoose.Schema({
   name: {
     type: String,
     required: [true, 'Please Add name to your Tag'],
-    unique: [true, 'Name in already in the database'],
-    maxlength: [52, 'Name connot be longer than 52']
+    unique: [true, 'Tag in already in the database'],
+    maxlength: [52, 'Name connot be longer than 52'],
   },
   slug: String,
   description: {
     type: String,
-    maxlength: [500, 'Description can not be more than 500 characters']
+    maxlength: [500, 'Description can not be more than 500 characters'],
   },
   color: {
     type: String,
-    default: '#6c96'
-  }
+    default: '#6c96',
+  },
 });
 
 // Create tag slug from the name
-TagSchema.pre('save', function(next) {
+TagSchema.pre('save', function (next) {
   this.slug = slugify(this.name, { lower: true });
   next();
 });
--- routes/tags.js
+++ routes/tags.js
@@ -1,42 +1,18 @@
-const express = require('express')
-let tags = require('../db/tags.js')
-const shortid = require('shortid')
+const express = require('express');
+let tags = require('../db/tags.js');
+const shortid = require('shortid');
 const {
   getTags,
   getTag,
-  searchTags,
   createTag,
   deleteTag,
-  updateTag
-} = require('../controllers/tags')
+  updateTag,
+} = require('../controllers/tags');
 
-const router = express.Router()
+const router = express.Router();
 
-// @route    GET /tags/search
-// @desc     search tegs
-// router.get('/search', (req, res) => {
-//   const filtered = tags.filter(tag => {
-//     return tag.name.indexOf(req.query.text) !== -1
-//   })
+router.route('/').get(getTags).post(createTag);
 
-//   res.send(filtered)
-// })
-
-router
-  .route('/')
-  .get(getTags)
-  .post(createTag)
-
-router
-  .route('/search')
-  .get(searchTags)
-
-router
-  .route('/:id')
-  .get(getTag)
-  .delete(deleteTag)
-  .put(updateTag)
-  
-
+router.route('/:id').get(getTag).delete(deleteTag).put(updateTag);
 
 module.exports = router;
