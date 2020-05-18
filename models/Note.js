ebca1369e077d7aa0c75b26e29b2223819d46c2f
hamidbakhtiari <hamid331994@gmail.com>
2020-05-18 19:13:42 +0430

    new route ( tag with notes ) added

--- models/Note.js
+++ models/Note.js
@@ -1,19 +1,21 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 const NoteSchema = new mongoose.Schema({
   body: {
     type: String,
-    required: [true, 'Please Enter a body text']
+    required: [true, 'Please Enter a body text'],
   },
   tag: {
     type: mongoose.Schema.ObjectId,
     ref: 'Tag',
-    required: true
+    required: [true, 'please add a tag to your note'],
   },
-  otherTags: [{
-    type: mongoose.Schema.ObjectId,
-    ref: 'Tag'
-  }]
-})
+  otherTags: [
+    {
+      type: mongoose.Schema.ObjectId,
+      ref: 'Tag',
+    },
+  ],
+});
 
-module.exports = mongoose.model('Note', NoteSchema)
+module.exports = mongoose.model('Note', NoteSchema);
--- routes/tags.js
+++ routes/tags.js
@@ -7,6 +7,7 @@ const {
   createTag,
   deleteTag,
   updateTag,
+  getTagAndNotes,
 } = require('../controllers/tags');
 
 const router = express.Router();
@@ -14,5 +15,7 @@ const router = express.Router();
 router.route('/').get(getTags).post(createTag);
 
 router.route('/:id').get(getTag).delete(deleteTag).put(updateTag);
+
+router.route('/:tagId/notes').get(getTagAndNotes);
 
 module.exports = router;
