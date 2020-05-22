4c1448118ac606a03578d26020ea8af9973020d0
hamidbakhtiari <hamid331994@gmail.com>
2020-05-22 16:12:42 +0430

    auth protect middleware | deleteuser route

--- models/User.js
+++ models/User.js
@@ -22,6 +22,11 @@ const UserSchema = new mongoose.Schema({
     minlength: [6, 'password is shorter than 6'],
     select: false,
   },
+  role: {
+    type: String,
+    enum: ['user', 'admin'],
+    default: 'user',
+  },
 });
 
 // Encrypt password using bcrypt
