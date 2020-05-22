65c905ec0af728757bed118cc0289ad2b6686266
hamidbakhtiari <hamid331994@gmail.com>
2020-05-22 15:24:37 +0430

    user model, routes, cntrollers

---
+++ models/User.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    maxlength: [256, "name can't be this long!"],
+  },
+  email: {
+    type: String,
+    required: [true, 'please add an email'],
+    unique: [true, 'email already exists'],
+    match: [
+      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+      'Please add a valid email',
+    ],
+  },
+  password: {
+    type: String,
+    required: [true, 'Please add a password'],
+    minlength: [6, 'password is shorter than 6'],
+    select: false,
+  },
+});
+
+// Encrypt password using bcrypt
+UserSchema.pre('save', async function () {
+  if (!this.isModified('password')) {
+    next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+// Sign JWT and return
+UserSchema.methods.getSignedJwtToken = function () {
+  return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRE,
+  });
+};
+
+// Match user entered password to hashed password in database
+UserSchema.methods.checkPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+module.exports = mongoose.model('User', UserSchema);
